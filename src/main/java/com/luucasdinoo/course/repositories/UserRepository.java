fe25c348f9371bd4bfc9497481ec76ea6e500156
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-06 16:05:38 -0300

    UserController, UserService, UserRepository

--- src/main/java/com/luucasdinoo/course/repositories/UserRepository.java
+++ src/main/java/com/luucasdinoo/course/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.luucasdinoo.course.repositories;
 
 import com.luucasdinoo.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/luucasdinoo/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.luucasdinoo.course.services;
+
+import com.luucasdinoo.course.entities.User;
+import com.luucasdinoo.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
