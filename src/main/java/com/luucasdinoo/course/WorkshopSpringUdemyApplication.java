8e8479a0640b4af28062141280b6d5f645db05e5
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-06 15:08:52 -0300

    Projeto criado!

---
+++ src/main/java/com/luucasdinoo/course/WorkshopSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.luucasdinoo.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopSpringUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luucasdinoo/course/WorkshopSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luucasdinoo.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
