cdbb44baab89432f4288ae619e99bd67a91939f6
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-07 15:48:27 -0300

    OrderItem associations pt.2

--- src/main/java/com/luucasdinoo/course/entities/OrderItem.java
+++ src/main/java/com/luucasdinoo/course/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.luucasdinoo.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.luucasdinoo.course.entities.pk.OrderItemPK;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -13,7 +14,7 @@ import java.util.Objects;
 public class OrderItem implements Serializable {
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
     private Integer quantity;
     private Double price;
 
@@ -25,6 +26,7 @@ public class OrderItem implements Serializable {
         this.quantity = quantity;
         this.price = price;
     }
+    @JsonIgnore
     public Order getOrder(){
         return  id.getOrder();
     }
---
+++ src/main/java/com/luucasdinoo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.luucasdinoo.course.repositories;
+
+import com.luucasdinoo.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
