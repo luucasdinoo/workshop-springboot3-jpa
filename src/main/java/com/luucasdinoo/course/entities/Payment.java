4fe61fada4ab5805720af9b0b3c2bb4101d5aeed
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-07 16:50:35 -0300

    Métodos subtotal e total

--- src/main/java/com/luucasdinoo/course/entities/Payment.java
+++ src/main/java/com/luucasdinoo/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.luucasdinoo.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Payment implements Serializable {
     private Instant moment;
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment(){}
